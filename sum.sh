23d96e8cb34bac8c95be30fa072b7c032c3d5dea
soni240 <s.fegade21@gmail.com>
2022-03-30 15:36:21 +0530

    sum

---
+++ sum.sh
@@ -0,0 +1,14 @@
+echo "Enter size(N)"
+read N
+
+sum=0
+
+echo "Enter Numbers"
+for((i=1;i<=N;i++))
+do
+        read num           #get number
+        sum=$((sum + num)) #sum+=num
+done
+
+echo $sum
+
